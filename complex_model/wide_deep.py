8e04aae88f11e3f1e931c0cc0a612eec549b472b
BestNico <Lzmkxumgh@gmail.com>
2021-06-11 15:57:51 +0900

    regress and wide-deep

---
+++ complex_model/wide_deep.py
@@ -0,0 +1,43 @@
+"""
+"""
+
+import tensorflow as ts
+from tensorflow import keras
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+housing = fetch_california_housing()
+
+X_train_full, X_test, y_train_full, y_test = train_test_split(
+    housing.data, housing.target)
+
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X_train_full, y_train_full)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_valid = scaler.transform(X_valid)
+X_test = scaler.transform(X_test)
+
+input_ = keras.layers.Input(shape=X_train.shape[1:])
+hidden1 = keras.layers.Dense(30, activation="relu")(input_)
+hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+concat = keras.layers.Concatenate()([input_, hidden2])
+output = keras.layers.Dense(1)(concat)
+model = keras.Model(inputs=[input_], outputs=[output])
+
+# check the result
+
+print(model.summary())
+
+# model.compile(loss="mean_squared_error", optimizer="sgd")
+# model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
+#
+# mse_test = model.evaluate(X_test, y_test)
+# X_new = X_test[:3]
+# y_pred = model.predict(X_new)
+
+
+
